053539474f51d43176584350c35569d94cff8d74
ikkiwar <ikkiofbodom@gmail.com>
2021-10-01 16:31:13 -0600

    api for properties sales

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'fbd82f3e35df59944ad6d91a37b225fd'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'fb73659a-cbbe-47de-add7-aa503d4c5e5a'
+};
